c9621647b5fa5c574247eb673763519e54518eae
jaimezwalker <jaimezwalker@gmail.com>
2017-08-31 12:18:38 +0800

    modify translation on tasks file

--- ingestigate texts and calls/ZH/Task0.py
+++ ingestigate texts and calls/ZH/Task0.py
@@ -1,15 +1,6 @@
-"""
-Python入门 项目 1, 任务 0
-
-完成该任务的文件中的每个任务. 以压缩文件提交整个文件夹或是GitHub repo。
-在项目准备页面上有完整的提交说明。
-"""
-
-
-"""
-读取短信与电话。
-如果你不知道如何读取文件，也是可以的
-你将在以后的课程中了解更多有关阅读文件的知识
+“””
+下面的文件将会从csv文件中读取读取短信与电话，
+你将在以后的课程中了解更多有关读取文件的知识。
 """
 import csv
 with open('texts.csv', 'r') as f:
@@ -25,7 +16,7 @@ with open('calls.csv', 'r') as f:
 任务0: 
 短信的第一条记录是什么？最后一条记录是什么？
 输出信息: 
-"第一条短信记录, <发送方电话号码> 给 <接收方电话号码> 在 <什么时间>发送了短信"
-"最后一条通话记录, <拨号方电话号码> 给 <接收方电话号码> 在 <什么时间>拨打了电话, 通话持续了 <多少> 秒"
+"First record of texts, <incoming number> texts <answering number> at time <time>"
+"Last record of calls, <incoming number> calls <answering number> at time <time>, lasting <during> seconds"
 """
 
