8dfc5b9dfc6e522a3a70bbac26195b25c2576335
jaimezwalker <jaimezwalker@gmail.com>
2017-09-06 10:54:56 +0800

    add init and return of start request

--- best movie/expanddouban.py
+++ best movie/expanddouban.py
@@ -2,10 +2,12 @@ import scrapy
 from scrapy import Selector
 from selenium import webdriver
 
-class MovieSpider(scrapy.Spider,url):
-    name = "movie_spider"
-    allowed_domains = ['douban.com']
-    start_url = [url]
+class MovieSpider(scrapy.Spider):
+    
+    def __init__(self, url):
+        name = "movie_spider"
+        allowed_domains = ['douban.com']
+        self.start_url = [url]
 
     def start_request(self):
         self.driver = webdriver.Firefox()
@@ -18,8 +20,9 @@ class MovieSpider(scrapy.Spider,url):
             except:
                 break
                 # parse the body your webdriver has
-                self.parse(driver.page_source)
+        html = self.parse(driver.page_source)
         driver.close()
+        return html
 
     def parse(self, body):
         # create Selector from html string
@@ -38,3 +41,6 @@ class MovieSpider(scrapy.Spider,url):
 def getHtml(url):
     movie = MovieSpider(url)
     return movie.start_request()
+
+douban_url = "https://movie.douban.com/tag/#/?sort=S&range=9,10&tags=%E7%94%B5%E5%BD%B1,%E7%BE%8E%E5%9B%BD,%E5%96%9C%E5%89%A7"
+print(getHtml(douban_url))
