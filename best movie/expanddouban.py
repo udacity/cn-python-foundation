f79a5aa2ae13d61561d9128d27a91c0b52f98d6a
jaimezwalker <jaimezwalker@gmail.com>
2017-09-19 11:13:58 +0800

    expanddouban.py

--- best movie/expanddouban.py
+++ best movie/expanddouban.py
@@ -1,19 +1,23 @@
 from selenium import webdriver
-from selenium.webdriver.support.ui import WebDriverWait
- 
- 
-def getHtml(url):
+import time 
+
+"""
+url: the douban page we will get html from
+loadmore: whether or not click load more on the bottom 
+waittime: seconds the broswer will wait after intial load and 
+""" 
+def getHtml(url, loadmore = False, waittime = 2):
     browser = webdriver.Chrome('chromedriver')
     browser.get(url)
-    time = 1 #second for the broswer to wait
-    WebDriverWait(browser, time)
-    while True:
-        try:
-            next_button = browser.find_element_by_class_name("more")
-            next_button.click()
-            print("click")
-        except:
-            break
+    time.sleep(waittime)
+    if loadmore:
+        while True:
+            try:
+                next_button = browser.find_element_by_class_name("more")
+                next_button.click()
+                time.sleep(waittime)
+            except:
+                break
     html = browser.page_source
     browser.quit()
     return html
@@ -21,4 +25,4 @@ def getHtml(url):
 # for test
 url = "https://movie.douban.com/tag/#/?sort=S&range=9,10&tags=电影,剧情,美国"
 html = getHtml(url)
-print(html)
+print(html) 
